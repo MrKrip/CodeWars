5eb65b777d33da4694878ec604b3619bc3e7f0b2
Veletiuk Pavlo <mrkripster4@gmail.com>
2025-03-10 15:12:34 +0200

    Done

---
+++ CodeWars/TimeFormat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    internal class TimeFormat
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            if (seconds > 359999)
+            {
+                seconds = 359999;
+            }
+            int Hours = seconds / 3600;
+            int Minutes = (seconds % 3600) / 60;
+            int Seconds = (seconds % 3600) % 60;
+            return String.Format("{0:d2}:{1:d2}:{2:d2}", Hours, Minutes, Seconds);
+        }
+    }
+}
